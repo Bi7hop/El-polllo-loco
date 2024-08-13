1b30c5781b4f1d94ac795f9c541792c2fbcaf151
Marcel Menke <marcel.menke1981@gmail.com>
2024-08-13 08:43:52 +0200

    added all graphic elements

--- models/world.class.js
+++ models/world.class.js
@@ -9,7 +9,11 @@ class World {
         new Cloud()
     ];
     backgroundObjects = [
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png')
+        new BackgroundObject('img/5_background/layers/air.png', 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
+
     ];
     canvas;
     ctx;
@@ -24,19 +28,10 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
+        this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
-
-        this.enemies.forEach(enemy => {
-            this.addToMap(enemy);
-        });
-
-        this.clouds.forEach(cloud => {
-            this.addToMap(cloud);
-        });
-
-        this.backgroundObjects.forEach((bgo) => {
-            this.addToMap(bgo);
-        });
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.clouds);
 
         let self = this;
         requestAnimationFrame(function() {
@@ -45,6 +40,11 @@ class World {
 
     }
     
+    addObjectsToMap(objects) {
+        objects.forEach(o => {
+            this.addToMap(o);
+        });
+    }
 
     addToMap(mo) {
         this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
