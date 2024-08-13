020191afe5c7a5ff3e6f98ff3cbffaf5e86b9ca3
Marcel Menke <marcel.menke1981@gmail.com>
2024-08-13 09:41:37 +0200

    animated pepe

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -10,3 +10,36 @@ class Chicken extends MovableObject {
     }
 
 }
+
+// class Chicken extends MovableObject {
+//     y = 355;
+//     height = 70;
+//     width = 75;
+//     images = [
+//         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+//         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+//         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+//     ];
+//     currentImage = 0;
+//     imageChangeCounter = 0;
+//     imageChangeInterval = 10; 
+
+//     constructor() {
+//         super().loadImage(this.images[this.currentImage]);
+
+//         this.x = 200 + Math.random() * 500;
+//         this.animate();
+//     }
+
+//     animate() {
+//         setInterval(() => {
+//             this.x -= 0.175;
+
+//             this.imageChangeCounter++;
+//             if (this.imageChangeCounter % this.imageChangeInterval === 0) {
+//                 this.currentImage = (this.currentImage + 1) % this.images.length;
+//                 this.loadImage(this.images[this.currentImage]);
+//             }
+//         }, 1000 / 60);
+//     }
+// }
