5a0d716444563bcfc60297b27215b856e963300a
Marcel Menke <marcel.menke1981@gmail.com>
2024-08-14 10:41:49 +0200

    added rectangle

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -31,6 +31,18 @@ class MovableObject {
         this.img.src = path;
     }
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        ctx.beginPath();
+        ctx.lineWidth = "4";
+        ctx.strokeStyle = "red";
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+    }
+
     loadImages(arr){
         arr.forEach((path) => {
         let img = new Image();
