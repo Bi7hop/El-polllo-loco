c42ab097e0768104acfdecbf245867f47d5a463c
Marcel Menke <marcel.menke1981@gmail.com>
2024-08-14 12:26:29 +0200

    added hurt animation

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -12,6 +12,8 @@ class MovableObject {
     acceleration = 2.5;
     energy = 100;
 
+    lastHit = 0;
+
 
     applyGravity() {
         setInterval(() => {
@@ -56,7 +58,15 @@ class MovableObject {
         this.energy -= 5;
         if (this.energy < 0) {
             this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
         }
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit;
+        timepassed = timepassed / 1000;
+        return timepassed < 1;
     }
 
     isDead() {
@@ -73,7 +83,7 @@ class MovableObject {
 }
 
     playAnimation(images) {
-        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
