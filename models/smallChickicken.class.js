ae13e95f0b688b8605ffd33226440dc62d856699
Marcel Menke <marcel.menke1981@gmail.com>
2024-08-16 11:03:27 +0200

    added small Chicken jump

--- models/smallChickicken.class.js
+++ models/smallChickicken.class.js
@@ -1,20 +1,23 @@
 class SmallChicken extends MovableObject {
-    y = 370; 
+    y = 370;
     height = 50;
     width = 50;
+    canJump = Math.random() < 0.5; // Nur 50% der Hühner können springen
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
     ];
+    acceleration = 5;  // Erhöhen Sie die Schwerkraft speziell für die kleinen Hühner
 
     constructor() {
         super().loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
 
-        this.x = 200 + Math.random() * 500;
+        this.x = 300 + Math.random() * 400;
         this.speed = 0.2 + Math.random() * 0.3;
 
+        this.applyGravity();
         this.animate();
     }
 
@@ -26,5 +29,21 @@ class SmallChicken extends MovableObject {
         setInterval(() => {
             this.playAnimation(this.IMAGES_WALKING);
         }, 150);
+
+        if (this.canJump) {
+            setInterval(() => {
+                if (Math.random() < 0.05 && !this.isAboveGround()) {
+                    this.jump();
+                }
+            }, 1000 / 60);
+        }
+    }
+
+    jump() {
+        this.speedY = 20;  // Kleinere Sprunghöhe speziell für die kleinen Hühner
+    }
+
+    isAboveGround() {
+        return this.y < 370;  // Überschreiben, um sicherzustellen, dass die kleinen Hühner wieder landen
     }
 }
