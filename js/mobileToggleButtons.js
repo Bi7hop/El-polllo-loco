041bc1830df9b9c9e1ac2005f26bfab38a184e1e
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-03 09:46:25 +0200

    added Jsdoc to mobileToggleButton.js

--- js/mobileToggleButtons.js
+++ js/mobileToggleButtons.js
@@ -1,3 +1,7 @@
+/**
+ * Event listener for DOMContentLoaded. Initializes the mobile button display logic
+ * and sets up event listeners for various UI elements.
+ */
 document.addEventListener('DOMContentLoaded', () => {
     const mobileButtonsContainer = document.getElementById('mobileButtons');
     const footer = document.getElementById('footer');
@@ -6,6 +10,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const musikButton = document.getElementById('mobileButtonMusik');
     const pauseButton = document.getElementById('mobileButtonPause');
 
+    /**
+     * Toggles the visibility of mobile buttons and footer based on the screen orientation,
+     * device type, and screen width.
+     */
     function toggleMobileButtons() {
         const isLandscape = window.matchMedia('(orientation: landscape)').matches;
         const isMobile = window.innerWidth < 1024;
@@ -20,6 +28,10 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    /**
+     * Updates the visibility of the footer based on the screen width and the display state
+     * of the startscreen.
+     */
     function updateFooterVisibility() {
         const isMobile = window.innerWidth < 1024;
 
@@ -32,24 +44,40 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    /**
+     * Event listener for the music button. Toggles the display between music and pause buttons
+     * and mutes/unmutes the sound using the soundManager.
+     */
     musikButton.addEventListener('click', () => {
         musikButton.style.display = 'none';
         pauseButton.style.display = 'block';
         soundManager.toggleMute();
     });
 
+      /**
+     * Event listener for the pause button. Toggles the display between pause and music buttons
+     * and mutes/unmutes the sound using the soundManager.
+     */
     pauseButton.addEventListener('click', () => {
         pauseButton.style.display = 'none';
         musikButton.style.display = 'block';
         soundManager.toggleMute();
     });
 
+     /**
+     * Event listener for the start button. Hides the startscreen and displays the game container.
+     * Updates the footer visibility accordingly.
+     */
     document.getElementById('startButton').addEventListener('click', () => {
         startscreen.style.display = 'none';
         gameContainer.style.display = 'block';
         updateFooterVisibility();
     });
 
+    /**
+     * Event listener for the back to start button. Hides the game container and displays the startscreen.
+     * Updates the footer visibility accordingly.
+     */
     document.getElementById('backToStartButton').addEventListener('click', () => {
         gameContainer.style.display = 'none';
         startscreen.style.display = 'block';
