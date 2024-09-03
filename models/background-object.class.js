14954dc89ec352b51681b826297c14e3dd3f1e34
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-03 11:38:18 +0200

    added Jsdoc to background-objects

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,7 +1,20 @@
+/**
+ * Class representing a background object in the game.
+ * Extends the MovableObject class to provide movement functionality.
+ */
 class BackgroundObject extends MovableObject {
 
+     /** @type {number} The width of the background object. */
     width = 720;
+
+    /** @type {number} The height of the background object. */
     height = 480;
+
+        /**
+     * Creates a new BackgroundObject instance.
+     * @param {string} imagePath - The path to the image file for the background object.
+     * @param {number} x - The initial x-position of the background object.
+     */
     constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.x = x;
