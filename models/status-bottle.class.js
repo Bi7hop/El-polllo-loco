eb21cb2531e8cbcc5421014f9e8da1462dcbd50d
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-03 12:32:36 +0200

    added Jsdoc to status-bottle.class.js

--- models/status-bottle.class.js
+++ models/status-bottle.class.js
@@ -1,10 +1,24 @@
+/**
+ * Class representing the status display for collected bottles in the game.
+ * Manages the rendering of the collected bottles on the game's status bar.
+ */
 class StatusBottle {
+
+    /**
+     * Creates a new StatusBottle instance.
+     * @param {Object} world - The game world object containing the current game state.
+     */
     constructor(world) {
         this.world = world;
         this.bottleImage = new Image();
         this.bottleImage.src = 'img/6_salsa_bottle/salsa_bottle.png'; 
     }
 
+    /**
+     * Draws the collected bottles on the game's status bar.
+     * @param {CanvasRenderingContext2D} ctx - The canvas rendering context where the bottles are drawn.
+     * @param {number} collectedBottles - The number of bottles that have been collected.
+     */
     drawCollectedBottles(ctx, collectedBottles) {
         const startX = 38; 
         const startY = 80;
