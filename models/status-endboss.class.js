9d3d91862f66c13a3900fa5389f73fb05ff20c5c
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-03 12:35:00 +0200

    added Jsdoc to status-endboss.class.js

--- models/status-endboss.class.js
+++ models/status-endboss.class.js
@@ -1,3 +1,7 @@
+/**
+ * Class representing the status bar for the endboss in the game.
+ * Extends the StatusBar class to display the endboss's health with a specific set of images.
+ */
 class EndbossStatusBar extends StatusBar {
     IMAGES = [
         'img/7_statusbars/2_statusbar_endboss/blue/blue0.png',
@@ -8,6 +12,10 @@ class EndbossStatusBar extends StatusBar {
         'img/7_statusbars/2_statusbar_endboss/blue/blue100.png',
     ];
 
+    /**
+     * Creates a new EndbossStatusBar instance.
+     * Initializes the status bar at the top right of the screen.
+     */
     constructor() {
         super();
         this.x = 0;
@@ -15,12 +23,21 @@ class EndbossStatusBar extends StatusBar {
         this.loadImages(this.IMAGES);
     }
 
+    /**
+     * Sets the world context for the status bar, including updating its position based on the canvas size.
+     * @param {Object} world - The game world object containing the current game state.
+     */
     setWorld(world) {
         this.world = world;
         this.canvasWidth = world.canvas.width;
         this.x = this.canvasWidth - 230;
     }
 
+    /**
+     * Draws the endboss's status bar on the canvas.
+     * Ensures that the image is fully loaded before drawing.
+     * @param {CanvasRenderingContext2D} ctx - The canvas rendering context where the status bar is drawn.
+     */
     draw(ctx) {
         if (this.img && this.img.complete) {  
             super.draw(ctx);
