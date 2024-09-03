275ba729fd8b8302cbe4cd8caeec1a031aa6d7d2
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-03 12:14:26 +0200

    added Jsdoc to level.class.js

--- models/level.class.js
+++ models/level.class.js
@@ -1,9 +1,19 @@
+/**
+ * Class representing a level in the game.
+ * Contains enemies, clouds, background objects, and other level-specific details.
+ */
 class Level {
    enemies;
    clouds;
    backgroundObjects;
    level_end_x = 3200;
 
+   /**
+     * Creates a new Level instance.
+     * @param {MovableObject[]} enemies - The array of enemies in the level.
+     * @param {MovableObject[]} clouds - The array of clouds in the level.
+     * @param {MovableObject[]} backgroundObjects - The array of background objects in the level.
+     */
    constructor(enemies, clouds, backgroundObjects){
         this.enemies = enemies;
         this.clouds = clouds;
