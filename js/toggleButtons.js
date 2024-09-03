fb20ec9acfe52754761d371dccfc61bd40ae75e9
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-03 09:48:50 +0200

    added Jsdoc to toggleButtons.js

--- js/toggleButtons.js
+++ js/toggleButtons.js
@@ -1,8 +1,16 @@
+/**
+ * Event listener for DOMContentLoaded. Initializes the game start logic
+ * and sets up event listeners for various UI elements.
+ */
 document.addEventListener('DOMContentLoaded', () => {
     const gameContainer = document.getElementById('gameContainer');
     const startScreen = document.getElementById('startscreen');
     const buttons = document.querySelectorAll('.game-button');
 
+    /**
+     * Hides game buttons after the game starts based on screen orientation and device type.
+     * Buttons are hidden on mobile devices in landscape mode, except for small devices.
+     */
     function hideButtonsAfterGameStart() {
         const isLandscape = window.matchMedia('(orientation: landscape)').matches;
         const isMobile = window.innerWidth < 1024 && window.innerHeight < window.innerWidth;
@@ -13,6 +21,10 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    /**
+     * Ensures that the game buttons are visible on the start screen based on screen orientation and device type.
+     * Buttons are shown unless the device is a mobile in landscape mode, except for small devices.
+     */
     function ensureButtonsVisibleOnStartScreen() {
         const isLandscape = window.matchMedia('(orientation: landscape)').matches;
         const isMobile = window.innerWidth < 1024 && window.innerHeight < window.innerWidth;
@@ -23,12 +35,20 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    /**
+     * Event listener for the start button. Displays the game container and hides the start screen.
+     * Hides the game buttons based on the current screen and device configuration.
+     */
     document.getElementById('startButton').addEventListener('click', () => {
         gameContainer.style.display = 'block';  
         startScreen.style.display = 'none';     
         hideButtonsAfterGameStart();
     });
 
+    /**
+     * Event listener for the back to start button. Hides the game container and shows the start screen.
+     * Ensures the game buttons are visible on the start screen.
+     */
     document.getElementById('backToStartButton').addEventListener('click', () => {
         gameContainer.style.display = 'none';  
         startScreen.style.display = 'flex';    
