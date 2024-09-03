4cb69e9d81b5f739b41ff1589bd7468db3539ae0
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-03 12:00:31 +0200

    added Jsdoc to collectables.class.js

--- models/collectables.class.js
+++ models/collectables.class.js
@@ -1,6 +1,15 @@
+/**
+ * Class representing a collectable object in the game.
+ * Extends the MovableObject class and provides basic functionality for loading images
+ * and playing animations.
+ */
 class Collectable extends MovableObject {
     currentImage = 0;
 
+    /**
+     * Creates a new Collectable instance.
+     * @param {string[]} imageArray - Array of image paths for the collectable's animation.
+     */
     constructor(imageArray) {
         super();
         this.loadImages(imageArray);
