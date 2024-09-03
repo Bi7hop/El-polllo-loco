34f80dfcc94226efd2d209f532692af3156a73ba
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-03 11:51:54 +0200

    added Jsdoc to chicken.class.js

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,3 +1,7 @@
+/**
+ * Class representing a chicken enemy in the game.
+ * Extends the MovableObject class and provides movement, animation, and collision handling.
+ */
 class Chicken extends MovableObject {
     static SPAWNED_POSITIONS = [];
     y = 355;
@@ -11,6 +15,9 @@ class Chicken extends MovableObject {
     IMAGE_DEAD = 'img/3_enemies_chicken/chicken_normal/2_dead/dead.png';
     removeAfterDeath = false;
 
+    /**
+     * Creates a new Chicken instance, initializes its position, speed, and animations.
+     */
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
@@ -45,11 +52,19 @@ class Chicken extends MovableObject {
         this.animate();
     }
     
-
+    
+    /**
+     * Checks if the chicken is hit by a throwable object.
+     * @param {ThrowableObject} throwableObject - The object to check for collision with.
+     * @returns {boolean} True if the chicken is hit, false otherwise.
+     */
     isHitBy(throwableObject) {
         return throwableObject instanceof ThrowableObject && this.isColliding(throwableObject);
     }
 
+    /**
+     * Handles the logic for when the chicken dies. Stops movement, changes image, and plays death sound.
+     */
     die() {
         this.energy = 0;
         this.speed = 0;
@@ -58,20 +73,34 @@ class Chicken extends MovableObject {
         this.scheduleRemoval();  
     }
 
+     /**
+     * Checks if the chicken is dead.
+     * @returns {boolean} True if the chicken is dead, false otherwise.
+     */
     isDead() {
         return this.energy === 0;
     }
 
+    /**
+     * Checks if the chicken is ready to be removed from the game after death.
+     * @returns {boolean} True if the chicken is removable, false otherwise.
+     */
     isRemovable() {
         return this.removeAfterDeath;
     }
 
+    /**
+     * Schedules the chicken to be removed from the game after a delay.
+     */
     scheduleRemoval() {
         setTimeout(() => {
             this.removeAfterDeath = true;
         }, 3000);  
     }
 
+    /**
+     * Starts the animation loops for moving and playing walking animations.
+     */
     animate() {
         setInterval(() => {
             if (!this.isDead()) {
