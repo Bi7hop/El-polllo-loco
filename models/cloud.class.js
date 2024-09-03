da2175a776970bc8a55de6cb0b2636a80ba668a6
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-03 11:54:33 +0200

    added Jsdoc to cloud.class.js

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,10 +1,16 @@
+/**
+ * Class representing a cloud in the game.
+ * Extends the MovableObject class and provides movement functionality.
+ */
 class Cloud extends MovableObject {
     y = 20;
     height = 250;
     width = 450;
     
 
-
+    /**
+     * Creates a new Cloud instance, initializes its position, and starts its movement animation.
+     */
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
 
@@ -12,6 +18,9 @@ class Cloud extends MovableObject {
         this.animate();
     }
 
+    /**
+     * Starts the cloud's movement animation, moving it to the left.
+     */
     animate() {
         this.moveLeft();
     }
