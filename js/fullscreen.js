e1ac5e1a2853e5ae43c4fcb5ec48f4ce75d2871f
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-04 09:25:40 +0200

    fixed fullscreen bug

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,27 +1,40 @@
 /**
- * Retrieves the game canvas and fullscreen button elements from the DOM.
+ * References to DOM elements for game canvas, fullscreen button, 
+ * startscreen, and gameContainer.
  */
 const gameCanvas = document.getElementById('canvas');
 const fullscreenButton = document.getElementById('fullscreenButton');
+const startscreen = document.getElementById('startscreen');
+const gameContainer = document.getElementById('gameContainer');
 
 /**
- * Adds an event listener to the fullscreen button to toggle fullscreen mode
- * for the game canvas. The function uses different methods depending on the browser.
+ * Toggles fullscreen mode when the fullscreen button is clicked.
+ * Depending on the browser, different methods are used to request fullscreen.
  */
 fullscreenButton.addEventListener('click', () => {
-    if (gameCanvas.requestFullscreen) {
-        gameCanvas.requestFullscreen();
-    } else if (gameCanvas.mozRequestFullScreen) { 
-        gameCanvas.mozRequestFullScreen();
-    } else if (gameCanvas.webkitRequestFullscreen) { 
-        gameCanvas.webkitRequestFullscreen();
-    } else if (gameCanvas.msRequestFullscreen) {
-        gameCanvas.msRequestFullscreen();
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+    } else {
+        if (gameCanvas.requestFullscreen) {
+            gameCanvas.requestFullscreen();
+        } else if (gameCanvas.mozRequestFullScreen) {
+            gameCanvas.mozRequestFullScreen();
+        } else if (gameCanvas.webkitRequestFullscreen) {
+            gameCanvas.webkitRequestFullscreen();
+        } else if (gameCanvas.msRequestFullscreen) {
+            gameCanvas.msRequestFullscreen();
+        }
     }
 });
 
 /**
- * Adds an event listener to the document that logs a message when the fullscreen mode changes.
+ * Listens for changes to fullscreen mode.
+ * Ensures that after exiting fullscreen, only the game container is visible,
+ * while the start screen is hidden.
  */
 document.addEventListener('fullscreenchange', () => {
+    if (!document.fullscreenElement) {
+        startscreen.style.display = 'none';  
+        gameContainer.style.display = 'flex';  
+    }
 });
