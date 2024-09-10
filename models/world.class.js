5c18bacfd664a20dd639bf7a0e608fcaa23928d7
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-10 09:02:52 +0200

    fixed bug splash ani

--- models/world.class.js
+++ models/world.class.js
@@ -411,10 +411,11 @@ drawGameObjects() {
     this.drawObjectsByType('clouds', this.level.clouds);
     this.drawObjectsByType('character', this.character);
     this.drawObjectsByType('throwableObjects', this.throwableObjects);
-    this.drawObjectsByType('coins', this.coins);
     this.drawBottles();
+    this.drawSplashAnimations(); 
     this.ctx.translate(-this.camera_x, 0);
 }
+
 
 /**
  * Draws all bottles and animates them.
