a1f70760c89585b23f87b09e341a85fc7ed3b55b
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-10 09:18:00 +0200

    refactoring cloud.class

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,6 +1,7 @@
 class Cloud extends MovableObject {
     velocity = { xMax: 0.15 };  
     acceleration = { x: 1 };   
+
     /**
      * Creates a new Cloud instance with randomized width, height, and position.
      * @param {number} width - The base width of the cloud.
@@ -10,16 +11,38 @@ class Cloud extends MovableObject {
     constructor(width, height, x) {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
         
-        this.width = width * (Math.random() + 1);  
-        this.height = height * (Math.random() + 1); 
-        this.x = x;  
-        
-        this.y = Math.ceil(Math.random() * 3) * 25; 
-
-        this.velocity.xMax = 0.15;  
-        this.acceleration.x = 1;    
+        this.setRandomDimensions(width, height);
+        this.setInitialPosition(x);
+        this.setInitialMovementProperties();
 
         this.animate();
+    }
+
+    /**
+     * Sets random dimensions for the cloud based on the provided base width and height.
+     * @param {number} baseWidth - The base width of the cloud.
+     * @param {number} baseHeight - The base height of the cloud.
+     */
+    setRandomDimensions(baseWidth, baseHeight) {
+        this.width = baseWidth * (Math.random() + 1);  
+        this.height = baseHeight * (Math.random() + 1); 
+    }
+
+    /**
+     * Sets the initial X and Y position for the cloud.
+     * @param {number} initialX - The initial X position of the cloud.
+     */
+    setInitialPosition(initialX) {
+        this.x = initialX;  
+        this.y = Math.ceil(Math.random() * 3) * 25;  
+    }
+
+    /**
+     * Sets the initial movement properties for the cloud.
+     */
+    setInitialMovementProperties() {
+        this.velocity.xMax = 0.15;  
+        this.acceleration.x = 1;    
     }
 
     /**
