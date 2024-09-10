76b1bf34fe0f54d1fb87f2cc021225d7774b424a
Marcel Menke <marcel.menke1981@gmail.com>
2024-09-10 14:15:45 +0200

    fixed startscreen bug

--- js/orientation.js
+++ js/orientation.js
@@ -1,17 +1,14 @@
-/**
- * Event listener for DOMContentLoaded. Initializes the orientation check
- * and sets up event listeners for window resize and orientation change.
- */
 document.addEventListener('DOMContentLoaded', () => {
     const rotateMessage = document.getElementById('rotateMessage');
     const gameContainer = document.getElementById('gameContainer');
     const startScreen = document.getElementById('startscreen');
 
+    let isGameActive = false; // Flag to track if the game is active
+
     /**
      * Checks the device's orientation and screen size to determine whether to display
-     * a message prompting the user to rotate their device. If the device is in portrait mode
-     * with a small width (e.g., on a mobile phone), the rotate message is shown and other
-     * containers are hidden.
+     * a message prompting the user to rotate their device. The rotate message is shown only
+     * if the device is in portrait mode with a small width (e.g., on a mobile phone).
      */
     function checkOrientation() {
         if (window.innerWidth < 600 && window.innerHeight > window.innerWidth) {
@@ -20,12 +17,34 @@ document.addEventListener('DOMContentLoaded', () => {
             startScreen.style.display = 'none';
         } else {
             rotateMessage.style.display = 'none';
-            startScreen.style.display = 'flex';
+            if (isGameActive) {
+                gameContainer.style.display = 'flex';
+                startScreen.style.display = 'none';
+            } else {
+                startScreen.style.display = 'flex';
+                gameContainer.style.display = 'none';
+            }
         }
     }
+
+    /**
+ * Starts the game by setting the game state to active, hiding the start screen,
+ * displaying the game container, and checking the current device orientation.
+ * 
+ * @function
+ */
+    function startGame() {
+        isGameActive = true; 
+        startScreen.style.display = 'none';
+        gameContainer.style.display = 'block';
+        checkOrientation(); 
+    }
+
+    
+    document.getElementById('startButton').addEventListener('click', startGame);
 
     window.addEventListener('resize', checkOrientation);
     window.addEventListener('orientationchange', checkOrientation);
 
-    checkOrientation();
+    checkOrientation(); 
 });
